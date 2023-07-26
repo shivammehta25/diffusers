f3ee6c930a8c719ef413a14740bdd830c3595872
Shivam Mehta <shivam.mehta25@gmail.com>
2023-07-26 23:28:49 +0000

    Adding some snake formatting

--- src/diffusers/models/attention.py
+++ src/diffusers/models/attention.py
@@ -205,8 +205,9 @@ class BasicTransformerBlock(nn.Module):
 
         return hidden_states
 
+
 class Snake(nn.Module):
-    '''         
+    """
     Implementation of the serpentine-like sine-based periodic activation function:
     .. math::
          Snake_a := x + \frac{1}{a} sin^2(ax) = x - \frac{1}{2a}cos{2ax} + \frac{1}{2a}
@@ -217,52 +218,53 @@ class Snake(nn.Module):
         - Input: (N, *) where * means, any number of additional
           dimensions
         - Output: (N, *), same shape as the input
-        
+
     Parameters:
         - a - trainable parameter
-    
+
     References:
         - This activation function is from this paper by Liu Ziyin, Tilman Hartwig, Masahito Ueda:
         https://arxiv.org/abs/2006.08195
-        
+
     Examples:
         >>> a1 = snake(256)
         >>> x = torch.randn(256)
         >>> x = a1(x)
-    '''
+    """
+
     def __init__(self, in_features, out_features, a=None, trainable=True):
-        '''
+        """
         Initialization.
         Args:
             in_features: shape of the input
             a: trainable parameter
             trainable: sets `a` as a trainable parameter
-            
-            `a` is initialized to 1 by default, higher values = higher-frequency, 
-            5-50 is a good starting point if you already think your data is periodic, 
-            consider starting lower e.g. 0.5 if you think not, but don't worry, 
+
+            `a` is initialized to 1 by default, higher values = higher-frequency,
+            5-50 is a good starting point if you already think your data is periodic,
+            consider starting lower e.g. 0.5 if you think not, but don't worry,
             `a` will be trained along with the rest of your model
-        '''
-        super(Snake,self).__init__()
-        self.in_features = in_features if isinstance(in_features, list) else [in_features]
+        """
+        super(Snake, self).__init__()
+        self.in_features = out_features if isinstance(out_features, list) else [out_features]
         self.proj = LoRACompatibleLinear(in_features, out_features)
         # Initialize `a`
         if a is not None:
-            self.a = nn.Parameter(torch.ones(self.in_features) * a) # create a tensor out of alpha
-        else:            
+            self.a = nn.Parameter(torch.ones(self.in_features) * a)  # create a tensor out of alpha
+        else:
             m = Exponential(torch.tensor([0.1]))
-            self.a = nn.Parameter((m.rsample(self.in_features)).squeeze()) # random init = mix of frequencies
+            self.a = nn.Parameter((m.rsample(self.in_features)).squeeze())  # random init = mix of frequencies
 
-        self.a.requiresGrad = trainable # set the training of `a` to true
+        self.a.requiresGrad = trainable  # set the training of `a` to true
 
     def forward(self, x):
-        '''
+        """
         Forward pass of the function.
         Applies the function to the input elementwise.
         Snake ∶= x + 1/a* sin^2 (xa)
-        '''
+        """
         x = self.proj(x)
-        return  x + (1.0/self.a) * torch.pow(torch.sin(x * self.a), 2)
+        return x + (1.0 / self.a) * torch.pow(torch.sin(x * self.a), 2)
 
 
 class FeedForward(nn.Module):
